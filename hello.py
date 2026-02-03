97b8b9c0555c3a0ba20dc6341415de44976b8fe5
shanmutp <shanmu@360ripples.com>
2026-02-03 13:36:09 +0530

    adding the third local change in local branch

--- hello.py
+++ hello.py
@@ -2,6 +2,7 @@
 # Local Python batch change 
 # Second local python change*****
 # Adding a man change and pushing to local main
+# Adding the third local python change***
 def add(a,b):
     a=(2*a)-b;
     return a+b;
